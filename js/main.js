d4020b8f7ecfd39660dff6ac896c7e2d1defe1f4
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2025-09-13 21:18:26 +0800

    test

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@
  * @build 13.09.25 @updated 21:42 PHT
 */
 console.log("🦄 deploying main modules");
-import { initHideOverlayOnload } from 'https://hymn-design.netlify.app/js/xxx.js?v=1.0.0';
+import { initHideOverlayOnload } from 'https://hymn-design.netlify.app/js/test.js?v=1.0.0';
+
+document.addEventListener("DOMContentLoaded", () => {
+    test();
+    console.log("✅ initialized test");
+});
