9f2db5b341ee3d76489374bc94f14b0106b3d35a
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2025-09-13 21:28:19 +0800

    ascii update

--- js/main.js
+++ js/main.js
@@ -3,14 +3,12 @@
  * @build 13.09.25 @updated 21:42 PHT
 */
 console.log("🦄 deploying main modules");
-import { test } from 'https://hymn-design.netlify.app/js/test.js?v=1.0.0';
 import { initASCII } from 'https://hymn-design.netlify.app/js/initASCII.js?v=1.0.0';
 
-document.addEventListener("DOMContentLoaded", () => {
-    test();
-    console.log("✅ initialized test");
-});
-
+/**
+ * HYMN | initASCII
+ * @build 02.10.21 @updated 23:32
+*/
 document.addEventListener("DOMContentLoaded", () => {
     initASCII();
     console.log("✅ initialized initASCII");
