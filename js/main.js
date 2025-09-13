cc4e060005ff91decb3072a5ca8b1532c9848ebe
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2025-09-13 21:07:52 +0800

    bsdge

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/**
+ * PPN LABS | modules
+ * @build 11.09.25 @updated 21:42 PHT
+*/
+console.log("🦄 deploying main modules");
+import { initHideOverlayOnload } from 'https://cdn.ppn-labs.com/js/initHideOverlayOnload.js?v=1.0.0';
