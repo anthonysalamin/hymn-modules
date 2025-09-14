16187a18c78f0d5249851f6dff77dc65af37e7f8
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2025-09-14 13:09:15 +0800

    join us scripts

---
+++ js/pages/static/join-us/initStickyListAnchor.js
@@ -0,0 +1,61 @@
+/**
+ * HYMN | initStickyListAnchor
+ * @build 11.09.24 @updated 22:28
+ */
+
+export function initStickyListAnchor() {
+    (function () {
+        function isMobile() {
+            return window.innerWidth <= 479;
+        }
+
+        function list_switchColorOnClick(event) {
+            let el = event.currentTarget;
+            if (!el.classList.contains("list-checked")) {
+                let checked = document.querySelectorAll(".list-checked");
+                checked.forEach(item => {
+                    item.classList.remove("list-checked");
+                });
+                el.classList.add("list-checked");
+            }
+        }
+
+        function sticky_list__scrollOnClick(OPTIONS) {
+            let triggers = document.querySelectorAll(OPTIONS.LIST_TRIGGERS);
+            triggers.forEach(trigger => {
+                trigger.addEventListener("click", ev => {
+                    list_switchColorOnClick(ev);
+                    let targetAttr = trigger.dataset.stickyList;
+                    let targetEl = document.querySelector(
+                        `[data-sticky-list-target="${targetAttr}"]`
+                    );
+                    let rect = targetEl.getBoundingClientRect();
+                    let scrollY = rect.top + window.pageYOffset - OPTIONS.OFFSET;
+
+                    gsap.to(window, {
+                        scrollTo: { y: scrollY, autoKill: false },
+                        duration: OPTIONS.DURATION,
+                        ease: OPTIONS.EASE
+                    });
+                });
+            });
+        }
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const OPTIONS = {
+                LIST_TRIGGERS: "[data-sticky-list]",
+                LIST_TARGETS: "[data-sticky-list-target]",
+                OFFSET: 150,
+                DURATION: 1.2,
+                EASE: "expo.inOut"
+            };
+
+            if (!document.querySelector(OPTIONS.LIST_TRIGGERS) || isMobile()) {
+                console.log("skipping sticky anchor handling");
+                return;
+            }
+
+            sticky_list__scrollOnClick(OPTIONS);
+        });
+    })();
+}
---
+++ js/pages/static/join-us/main.js
@@ -0,0 +1,14 @@
+/**
+ * HYMN | modules
+ * @build 14.09.25 @updated 21:42 PHT
+*/
+console.log("🦄 deploying join-us modules");
+
+import { initLottie } from 'https://hymn-design.netlify.app/js/pages/static/join-us/initLottie.js?v=1.0.0';
+
+/**
+ * HYMN | join-us
+ * @build 01.12.24 @updated 13:02
+ */
+join-us();
+console.log("✅ initialized join-us");
