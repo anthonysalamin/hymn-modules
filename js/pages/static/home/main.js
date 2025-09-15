ddfe18f8c4b23f3c543d656e31d4dc285d8250a7
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2025-09-15 12:29:38 +0800

    Update main.js

--- js/pages/static/home/main.js
+++ js/pages/static/home/main.js
@@ -3,7 +3,10 @@
  * @build 14.09.25 @updated 21:42 PHT
 */
 
-console.log("🦄 deploying home modules");
+console.log(
+    "%c🦄 deploying home modules",
+    "color: white; background: purple; padding: 2px 6px; border-radius: 3px;",
+  );
 
 import { initLottie } from 'https://hymn-design.netlify.app/js/pages/static/home/initLottie.js?v=1.0.0';
 
