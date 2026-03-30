b8a755aa2ecf3eb16427b356a69ca8b4a336ab90
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-03-30 13:44:34 +0800

    Update main.js

--- js/pages/static/agence/main.js
+++ js/pages/static/agence/main.js
@@ -8,13 +8,13 @@ console.log(
   "color: white; background: purple; padding: 2px 6px; border-radius: 3px;",
 );
 
-import { initClientMarquee } from 'https://cdn.hymn.design/js/pages/static/agence/initClientMarquee.js?v=1.0.0';
+//import { initClientMarquee } from 'https://cdn.hymn.design/js/pages/static/agence/initClientMarquee.js?v=1.0.0';
 import { initClickDrag } from 'https://cdn.hymn.design/js/pages/static/agence/initClickDrag.js?v=1.0.0';
 import { initMethodSwitch } from 'https://cdn.hymn.design/js/pages/static/agence/initMethodSwitch.js?v=1.0.0';
-//import { initMarkeeMWG008 } from 'https://cdn.hymn.design/js/pages/static/agence/initMarkeeMWG008.js?v=1.0.0';
+import { initMarkeeMWG008 } from 'https://cdn.hymn.design/js/pages/static/agence/initMarkeeMWG008.js?v=1.0.0';
 
 // core
-initClientMarquee();
+//initClientMarquee();
 initClickDrag();
 initMethodSwitch();
 
