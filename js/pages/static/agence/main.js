271d5669bff9f03ece23db1595809476edbed31e
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-03-30 13:40:47 +0800

    Update main.js

--- js/pages/static/agence/main.js
+++ js/pages/static/agence/main.js
@@ -8,24 +8,25 @@ console.log(
   "color: white; background: purple; padding: 2px 6px; border-radius: 3px;",
 );
 
-// import { initClientMarquee } from 'https://cdn.hymn.design/js/pages/static/agence/initClientMarquee.js?v=1.0.0';
+import { initClientMarquee } from 'https://cdn.hymn.design/js/pages/static/agence/initClientMarquee.js?v=1.0.0';
 import { initClickDrag } from 'https://cdn.hymn.design/js/pages/static/agence/initClickDrag.js?v=1.0.0';
 import { initMethodSwitch } from 'https://cdn.hymn.design/js/pages/static/agence/initMethodSwitch.js?v=1.0.0';
-import { initMarkeeMWG008 } from 'https://cdn.hymn.design/js/pages/static/agence/initMarkeeMWG008.js?v=1.0.0';
+//import { initMarkeeMWG008 } from 'https://cdn.hymn.design/js/pages/static/agence/initMarkeeMWG008.js?v=1.0.0';
 
 // on DOM loaded
 document.addEventListener("DOMContentLoaded", () => {
 
   // core
-  // initClientMarquee();
+  initClientMarquee();
   initClickDrag();
   initMethodSwitch();
-
-  const marqueeRoot = document.querySelector('[data-mwg008="root"]');
-  if (marqueeRoot) {
-    initMarkeeMWG008(marqueeRoot);
-    console.log(`✅ Initialized markee MWG008`);
-  }
+  /*
+    const marqueeRoot = document.querySelector('[data-mwg008="root"]');
+    if (marqueeRoot) {
+      initMarkeeMWG008(marqueeRoot);
+      console.log(`✅ Initialized markee MWG008`);
+    }
+    */
 
   // font-dependent
   document.fonts.ready.then(() => {
