5fb43534b29270e58a76cf299e139f2236ca947e
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-03-30 11:26:31 +0800

    added marquee mwg008 js and main

--- js/pages/static/agence/main.js
+++ js/pages/static/agence/main.js
@@ -11,8 +11,10 @@ console.log(
 import { initClientMarquee } from 'https://cdn.hymn.design/js/pages/static/agence/initClientMarquee.js?v=1.0.0';
 import { initClickDrag } from 'https://cdn.hymn.design/js/pages/static/agence/initClickDrag.js?v=1.0.0';
 import { initMethodSwitch } from 'https://cdn.hymn.design/js/pages/static/agence/initMethodSwitch.js?v=1.0.0';
+import { initMarkeeMWG008 } from 'https://cdn.hymn.design/js/pages/static/agence/initMarkeeMWG008.js?v=1.0.0';
 
 initClientMarquee();
 initClickDrag();
 initMethodSwitch();
+initMarkeeMWG008();
 console.log("✅ All agence modules initialized successfully");
