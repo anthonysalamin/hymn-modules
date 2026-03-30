76247f88438027d6b2816786b38be1c2caa3a2ae
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-03-30 13:41:52 +0800

    Update main.js

--- js/pages/static/agence/main.js
+++ js/pages/static/agence/main.js
@@ -13,13 +13,14 @@ import { initClickDrag } from 'https://cdn.hymn.design/js/pages/static/agence/in
 import { initMethodSwitch } from 'https://cdn.hymn.design/js/pages/static/agence/initMethodSwitch.js?v=1.0.0';
 //import { initMarkeeMWG008 } from 'https://cdn.hymn.design/js/pages/static/agence/initMarkeeMWG008.js?v=1.0.0';
 
+// core
+initClientMarquee();
+initClickDrag();
+initMethodSwitch();
+
 // on DOM loaded
 document.addEventListener("DOMContentLoaded", () => {
 
-  // core
-  initClientMarquee();
-  initClickDrag();
-  initMethodSwitch();
   /*
     const marqueeRoot = document.querySelector('[data-mwg008="root"]');
     if (marqueeRoot) {
